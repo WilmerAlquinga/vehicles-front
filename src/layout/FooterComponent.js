0466194751fa649cad49aa7da7b84281b33cdab6
wsalquinga <wilmer_alquinga@outlook.es>
2023-10-31 06:22:04 -0500

    Initial commit

---
+++ src/layout/FooterComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const FooterComponent = () => {
+  return (
+    <div>
+      <footer className="footer">
+        <span>Sistema de gestión de vehiculos</span>
+      </footer>
+    </div>
+  );
+};
+
+export default FooterComponent;
